41f85286cfbe2390183ad9cff5b1e200723d3e2d
Tanuj M <thetanuj@hotmail.com>
2020-07-10 18:42:28 +0530

    Getting the list of apps

--- app/src/main/java/app/olauncher/MainActivity.kt
+++ app/src/main/java/app/olauncher/MainActivity.kt
@@ -1,11 +1,38 @@
 package app.olauncher
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.ResolveInfo
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                // Hide the nav bar and status bar
+//                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    private fun getAppsList() {
+        val pm = packageManager
+        val intent = Intent(Intent.ACTION_MAIN, null)
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+        val apps = pm.queryIntentActivities(intent, 0)
+        for (a: ResolveInfo in apps) {
+            Log.d("TAG", a.loadLabel(pm).toString())
+            Log.d("PACKAGE", a.activityInfo.packageName)
+        }
     }
 }
