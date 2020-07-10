3d341d9a0282bec718ebd3594ab17375eb2e7fd6
Tanuj M <thetanuj@hotmail.com>
2020-07-10 23:01:19 +0530

    Added main fragment with nav_graph

---
+++ app/src/main/java/app/olauncher/MainFragment.kt
@@ -0,0 +1,29 @@
+package app.olauncher
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+}
---
+++ app/src/main/java/app/olauncher/MainViewModel.kt
@@ -0,0 +1,7 @@
+package app.olauncher
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
