7fbe65ad65f669f2a31922631b9cf6401d116bbf
Tanuj M <thetanuj@hotmail.com>
2020-07-12 01:09:47 +0530

    Show/hide keyboard based on app list scrolling

--- app/src/main/java/app/olauncher/AppListFragment.kt
+++ app/src/main/java/app/olauncher/AppListFragment.kt
@@ -1,15 +1,19 @@
 package app.olauncher
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_app.*
+
 
 class AppListFragment : Fragment() {
 
@@ -38,6 +42,30 @@ class AppListFragment : Fragment() {
             adapter = appAdapter
         }
 
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            var isKeyboardDismissedByScroll = false
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                when (newState) {
+                    RecyclerView.SCROLL_STATE_DRAGGING -> {
+                        if (!isKeyboardDismissedByScroll) {
+                            search.hideKeyboard()
+                            isKeyboardDismissedByScroll = !isKeyboardDismissedByScroll
+                        }
+                    }
+                    RecyclerView.SCROLL_STATE_IDLE -> {
+                        isKeyboardDismissedByScroll = false
+                        if (!recyclerView.canScrollVertically(-1)) {
+                            search.requestFocus()
+                            search.showKeyboard()
+                        }
+                    }
+                }
+            }
+        })
+
         search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean = false
 
@@ -46,5 +74,28 @@ class AppListFragment : Fragment() {
                 return false
             }
         })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        search.requestFocus()
+        search.showKeyboard()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        search.clearFocus()
+        search.hideKeyboard()
+        activity?.onBackPressed()
+    }
+
+    fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
+
+    fun View.showKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
     }
 }
