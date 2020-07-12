61e33ab7ebf0c57c947bb289a5a75b4a0bd701f4
Tanuj M <thetanuj@hotmail.com>
2020-07-12 19:40:15 +0530

    Showing status bar on home screen

--- app/src/main/java/app/olauncher/MainActivity.kt
+++ app/src/main/java/app/olauncher/MainActivity.kt
@@ -1,7 +1,6 @@
 package app.olauncher
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 
@@ -15,17 +14,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) hideSystemUI()
-    }
-
-    private fun hideSystemUI() {
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                // Hide the nav bar and status bar
-//                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
