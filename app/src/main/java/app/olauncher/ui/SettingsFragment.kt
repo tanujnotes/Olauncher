12a5ac263fbf6085b1afecc743ae307af8ea0ccc
Tanuj M <thetanuj@hotmail.com>
2020-10-09 21:28:32 +0530

    Added option to remove settings permission when double tap lock is turned off

--- app/src/main/java/app/olauncher/ui/SettingsFragment.kt
+++ app/src/main/java/app/olauncher/ui/SettingsFragment.kt
@@ -167,7 +167,8 @@ class SettingsFragment : Fragment(), View.OnClickListener, View.OnLongClickListe
             deviceManager.removeActiveAdmin(componentName)
             prefs.lockModeOn = false
             populateLockSettings()
-            showToastShort(requireContext(), "Admin permission removed")
+            openEditSettingsPermission()
+            showToastLong(requireContext(), "Admin permission removed. You can remove settings permission too.")
         } else {
             val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
             intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName)
