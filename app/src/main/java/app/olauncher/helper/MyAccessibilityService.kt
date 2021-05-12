7a19ab8c3627fc01c41aee409708d4f4127fd231
Tanuj M <thetanuj@hotmail.com>
2021-05-12 12:19:49 +0530

    Running accessability service in a different process and start sticky

--- app/src/main/java/app/olauncher/helper/MyAccessibilityService.kt
+++ app/src/main/java/app/olauncher/helper/MyAccessibilityService.kt
@@ -1,6 +1,7 @@
 package app.olauncher.helper
 
 import android.accessibilityservice.AccessibilityService
+import android.content.Intent
 import android.os.Build
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
@@ -10,19 +11,26 @@ import app.olauncher.data.Prefs
 
 class MyAccessibilityService : AccessibilityService() {
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY
+    }
+
     override fun onServiceConnected() {
         Prefs(applicationContext).lockModeOn = true
-        showToastShort(this, "Double tap to lock enabled")
         super.onServiceConnected()
     }
 
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
-        val source: AccessibilityNodeInfo = event.source ?: return
-        if ((source.className == "android.widget.FrameLayout") and
-            (source.contentDescription == getString(R.string.lock_layout_description))
-        )
-            performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN)
+        try {
+            val source: AccessibilityNodeInfo = event.source ?: return
+            if ((source.className == "android.widget.FrameLayout") and
+                (source.contentDescription == getString(R.string.lock_layout_description))
+            )
+                performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN)
+        } catch (e: Exception) {
+            return
+        }
     }
 
     override fun onInterrupt() {
