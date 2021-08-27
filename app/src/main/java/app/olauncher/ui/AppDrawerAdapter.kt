17c2a1e1ac672ffadd000653ff70b16b40771443
Tanuj <tanbook@Tanujs-MBP.domain.name>
2021-08-27 23:57:35 +0530

    Changes in app filter to potentially fix crashes

--- app/src/main/java/app/olauncher/ui/AppDrawerAdapter.kt
+++ app/src/main/java/app/olauncher/ui/AppDrawerAdapter.kt
@@ -21,6 +21,7 @@ class AppDrawerAdapter(
     private val appHideListener: (Int, AppModel) -> Unit
 ) : RecyclerView.Adapter<AppDrawerAdapter.ViewHolder>(), Filterable {
 
+    private var appFilter = createAppFilter()
     var appsList: MutableList<AppModel> = mutableListOf()
     var appFilteredList: MutableList<AppModel> = mutableListOf()
 
@@ -51,12 +52,13 @@ class AppDrawerAdapter(
 
     override fun getItemCount(): Int = appFilteredList.size
 
-    // Filter app search results
-    override fun getFilter(): Filter {
+    override fun getFilter(): Filter = this.appFilter
+
+    private fun createAppFilter(): Filter {
         return object : Filter() {
             override fun performFiltering(constraint: CharSequence?): FilterResults {
                 val searchChars = constraint.toString()
-                appFilteredList = (if (searchChars.isEmpty()) appsList
+                val appFilteredList = (if (searchChars.isEmpty()) appsList
                 else appsList.filter { app -> appLabelMatches(app.appLabel, searchChars) } as MutableList<AppModel>)
 
                 val filterResults = FilterResults()
@@ -75,8 +77,8 @@ class AppDrawerAdapter(
     private fun appLabelMatches(appLabel: String, searchChars: String): Boolean {
         return (appLabel.contains(searchChars, true) or
                 Normalizer.normalize(appLabel, Normalizer.Form.NFD)
-                        .replace(Regex("\\p{InCombiningDiacriticalMarks}+"), "")
-                        .replace(Regex("[-_+,. ]"), "")
+                    .replace(Regex("\\p{InCombiningDiacriticalMarks}+"), "")
+                    .replace(Regex("[-_+,. ]"), "")
                     .contains(searchChars, true))
     }
 
@@ -94,7 +96,13 @@ class AppDrawerAdapter(
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val appHideButton: TextView = itemView.appHide
 
-        fun bind(flag: Int, appLabelGravity: Int, appModel: AppModel, listener: (AppModel) -> Unit, appInfoListener: (AppModel) -> Unit) =
+        fun bind(
+            flag: Int,
+            appLabelGravity: Int,
+            appModel: AppModel,
+            listener: (AppModel) -> Unit,
+            appInfoListener: (AppModel) -> Unit
+        ) =
             with(itemView) {
                 appHideLayout.visibility = View.GONE
                 appHideButton.text = (if (flag == Constants.FLAG_HIDDEN_APPS) "SHOW" else "HIDE")
