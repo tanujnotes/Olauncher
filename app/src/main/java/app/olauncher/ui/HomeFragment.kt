621f08967d0eb9044cf5174b53c52f8fe4611a7b
Tanuj <tanbook@Tanujs-MacBook-Pro.local>
2021-09-20 20:19:30 +0530

    Fixed status bar color for light theme

--- app/src/main/java/app/olauncher/ui/HomeFragment.kt
+++ app/src/main/java/app/olauncher/ui/HomeFragment.kt
@@ -314,17 +314,9 @@ class HomeFragment : Fragment(), View.OnClickListener, View.OnLongClickListener 
             requireActivity().window.insetsController?.show(WindowInsets.Type.statusBars())
         else
             @Suppress("DEPRECATION", "InlinedApi")
-            if (requireContext().isDarkThemeOn())
-                requireActivity().window.decorView.apply {
-                    systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                }
-            else
-                requireActivity().window.decorView.apply {
-                    systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or
-                            View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-                }
+            requireActivity().window.decorView.apply {
+                systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            }
     }
 
     private fun hideStatusBar() {
--- app/src/main/java/app/olauncher/ui/SettingsFragment.kt
+++ app/src/main/java/app/olauncher/ui/SettingsFragment.kt
@@ -245,17 +245,9 @@ class SettingsFragment : Fragment(), View.OnClickListener, View.OnLongClickListe
             requireActivity().window.insetsController?.show(WindowInsets.Type.statusBars())
         else
             @Suppress("DEPRECATION", "InlinedApi")
-            if (requireContext().isDarkThemeOn())
-                requireActivity().window.decorView.apply {
-                    systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                }
-            else
-                requireActivity().window.decorView.apply {
-                    systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or
-                            View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-                }
+            requireActivity().window.decorView.apply {
+                systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            }
     }
 
     private fun hideStatusBar() {
