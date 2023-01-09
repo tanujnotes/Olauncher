8dc9e483428939f2216b8dd4bcf140b495ba7e9b
Tanuj <thetanuj@hotmail.com>
2023-01-09 17:14:50 +0530

    Minor change

--- app/src/main/java/app/olauncher/ui/AppDrawerFragment.kt
+++ app/src/main/java/app/olauncher/ui/AppDrawerFragment.kt
@@ -9,7 +9,6 @@ import android.view.animation.AnimationUtils
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import androidx.appcompat.widget.SearchView
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -87,9 +86,9 @@ class AppDrawerFragment : Fragment() {
 
             override fun onQueryTextChange(newText: String): Boolean {
                 try {
-                    binding.appRename.isVisible = rename && newText.isNotBlank()
-                    binding.appDrawerTip.visibility = View.GONE
                     appAdapter.filter.filter(newText.trim())
+                    binding.appDrawerTip.visibility = View.GONE
+                    binding.appRename.visibility = if (rename && newText.isNotBlank()) View.VISIBLE else View.GONE
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
