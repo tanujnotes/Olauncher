311d10e319c53da887154e61c12c3aeb7da30ecb
Tanuj <thetanuj@hotmail.com>
2023-03-24 17:45:54 +0530

    Added select app hint

--- app/src/main/java/app/olauncher/ui/AppDrawerFragment.kt
+++ app/src/main/java/app/olauncher/ui/AppDrawerFragment.kt
@@ -50,7 +50,10 @@ class AppDrawerFragment : Fragment() {
     }
 
     private fun initViews() {
-        if (flag == Constants.FLAG_HIDDEN_APPS) binding.search.queryHint = "Hidden apps"
+        if (flag == Constants.FLAG_HIDDEN_APPS)
+            binding.search.queryHint = "Hidden apps"
+        else if (flag in Constants.FLAG_SET_HOME_APP_1..Constants.FLAG_SET_SWIPE_RIGHT_APP)
+            binding.search.queryHint = "Please select an app"
         if (canRename && prefs.renameTipShown.not()) {
             binding.appDrawerTip.text = getString(R.string.tip_start_typing_for_rename)
             binding.appDrawerTip.visibility = View.VISIBLE
