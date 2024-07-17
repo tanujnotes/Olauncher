d974234ec04f4b5c4219c9154d43b6dca12d83c4
J-cart <j.t.cartoonist@gmail.com>
2024-07-17 12:46:13 +0100

    (feat) app drawer character list

---
+++ app/src/main/java/app/olauncher/data/DrawerCharacterModel.kt
@@ -0,0 +1,7 @@
+package app.olauncher.data
+
+data class DrawerCharacterModel(
+    val character: String = "",
+    val inRange: Boolean = false,
+
+)
---
+++ app/src/main/java/app/olauncher/ui/DrawerCharacterAdapter.kt
@@ -0,0 +1,69 @@
+package app.olauncher.ui
+
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import app.olauncher.R
+import app.olauncher.data.DrawerCharacterModel
+import app.olauncher.databinding.DrawerAlphabetBinding
+
+class DrawerCharacterAdapter :
+    ListAdapter<DrawerCharacterModel, DrawerCharacterAdapter.ViewHolder>(diffObject) {
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = DrawerAlphabetBinding.bind(view)
+        fun bind(character: DrawerCharacterModel) {
+
+           if (character.inRange) {
+                binding.character.setTextColor(itemView.context.getColor(R.color.design_default_color_secondary))
+            }else{
+               val typedValue = TypedValue()
+               itemView.context.theme.resolveAttribute(R.attr.primaryColor, typedValue, true)
+               binding.character.setTextColor(itemView.context.getColor(typedValue.resourceId))
+            }
+
+            binding.character.text = character.character
+            binding.root.setOnClickListener {
+                listener?.let { it(character.character) }
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.drawer_alphabet, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val pos = getItem(position)
+        holder.bind(pos)
+    }
+
+    companion object {
+        val diffObject = object : DiffUtil.ItemCallback<DrawerCharacterModel>() {
+            override fun areItemsTheSame(
+                oldItem: DrawerCharacterModel,
+                newItem: DrawerCharacterModel
+            ): Boolean {
+                return oldItem.character == newItem.character
+            }
+
+            override fun areContentsTheSame(
+                oldItem: DrawerCharacterModel,
+                newItem: DrawerCharacterModel
+            ): Boolean {
+                return oldItem.hashCode() == newItem.hashCode()
+            }
+        }
+    }
+
+    private var listener: ((String) -> Unit)? = null
+    fun adapterClick(listener: (String) -> Unit) {
+        this.listener = listener
+    }
+}
